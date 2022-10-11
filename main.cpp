60a89da1304be76402cb4f46461345be058b95c3
jasonme0830 <2796931057@qq.com>
2022-10-11 20:11:53 +0800

    main cpp modify

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
+#include "bin/headline.h"
 
 using namespace std;
 
 int main()
 {
     cout<<"hello c++ world!"<<endl;
-
-
+    int res = sum(1,1);
+    cout<<res<<endl;
 
     return 0;
 }
