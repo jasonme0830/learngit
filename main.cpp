22840f1dd14219600c1f1cb6f20152c46a0ccf19
jasonme0830 <2796931057@qq.com>
2022-10-18 20:31:17 +0800

    keep touch

--- main.cpp
+++ main.cpp
@@ -12,15 +12,8 @@ struct TreeNode{
 int main()
 {
     cout<<"hello c++ world!"<<endl;
-<<<<<<< HEAD
     cout<<"1111111"<<endl;
 
-=======
-    int res = sum(1,1);
-    cout<<res<<endl;
-
-    cout<<"what you need?"<<endl;
->>>>>>> 89f45d71955d5faf4dda547e9023f04f573e189e
 
     return 0;
 }
