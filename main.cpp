dac98b3e6c28a764f0c0e98ae01a80be48194fe1
jasonme0830 <2796931057@qq.com>
2022-10-18 16:40:47 +0800

    add struct tree node to main

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include <string>
 
 using namespace std;
+struct TreeNode{
+    int data;
+    TreeNode* lchild;
+    TreeNode* rchild;
+};
 
 int main()
 {
     cout<<"hello c++ world!"<<endl;
-
+    cout<<"1111111"<<endl;
 
 
     return 0;
