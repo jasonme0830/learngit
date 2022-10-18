79d4b3f7bb43fbd7c155bac6fc4ddffb520449c0
jasonme0830 <2796931057@qq.com>
2022-10-18 21:18:05 +0800

    retore

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "bin/headline.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"hello c++ world!"<<endl;
+    int res = sum(1,1);
+    cout<<res<<endl;
+
+    cout<<"what you need?"<<endl;
+
+    return 0;
+}
